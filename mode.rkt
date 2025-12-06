e184610bd5d8005a1e6cd523187504fdd81fdc3e
Srikar Kavali <srikar.kavali@gmail.com>
2025-12-06 05:07:38 -0600

    Fix typo in current-command-line-arguments

--- mode.rkt
+++ mode.rkt
@@ -2,9 +2,9 @@
 (provide interactive?)
 
 (define interactive?
-  (let ([args (current-command-line-arguements)])
+  (let ([args (current-command-line-arguments)])
     (cond
-     [( (vector-length args) 0) #t]
+     [(= (vector-length args) 0) #t]
      [(string=? (vector-ref args 0) "-b") #f]
      [(string=? (vector-ref args 0) "--batch") #f]
      [else #t])))
